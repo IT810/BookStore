0dffd4cbf2fee06231bed63a37c517f38bc185a1
Hung Nguyen <hung.187pm20486@vanlanguni.vn>
2022-01-25 16:54:18 +0700

    Update Project

---
+++ BookStore.Model/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Model.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ BookStore.Model/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Model.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+
+        public virtual List<Book> Books { get; set; }
+
+        public Category()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+}
